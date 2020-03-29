32634b4d69fa3443bd06849a1a3d8dd521c941e9
Pedro <pfonseca@synopsys.com>
2020-03-29 12:25:49 +0100

    Linted 'application.py' + enabled incoming connections to workzeug

--- application.py
+++ application.py
@@ -1,16 +1,13 @@
 import config
 import logging
-import json
 from os import environ
 from flask import (
     request
-    , jsonify
- ) #,send_from_directory, render_template, jsonify, make_response
+ )
 from config import (
-    logger
-    , app, db, Result
-    ,Tester
+    logger, app, db, Result, Tester
 )
+
 
 @app.route('/testinsertone', methods=['POST'])
 def testInsertOne():
@@ -18,9 +15,9 @@ def testInsertOne():
     msg = None
     stat = Result.SUCCESS
     try:
-        
+
         data = request.get_json()
-        
+
         if data.get("message"):
             record = Tester(data.get("message"))
             db.session.add(record)
@@ -29,45 +26,52 @@ def testInsertOne():
             msg = record.toJSON()
 
     except Exception as ex:
+        logger.error(str(ex))
         pass
-    
-    return Result(data = msg, status = stat).toJSON()
-    
-@app.route('/test', methods=[ 'GET','POST'])
+
+    return Result(data=msg, status=stat).toJSON()
+
+
+@app.route('/test', methods=['GET', 'POST'])
 def test():
 
     msg = None
     stat = Result.SUCCESS
 
     if request.method == "GET":
-        msg ="{}".format(request.method)
-        
-    if request.method == "POST":
-        msg ="{}".format(request.method)
-        
-    return Result(data = msg, status = stat).toJSON()
+        msg = "{}".format(request.method)
 
-@app.route('/createtables', methods=[ 'POST'])
+    if request.method == "POST":
+        msg = "{}".format(request.method)
+
+    return Result(data=msg, status=stat).toJSON()
+
+
+@app.route('/createtables', methods=['POST'])
 def resetTables():
 
     msg = None
     stat = None
     try:
         logger.warn("CREATING TABLES")
-        db.create_all()    
+        db.create_all()
         msg = "Non existent tables created!"
         stat = Result.SUCCESS
     except Exception as ex:
         msg = str(ex)
         stat = Result.FAILURE
-        
-    return Result(data = msg, status = stat ).toJSON()    
+
+    return Result(data=msg, status=stat).toJSON()
+
 
 if __name__ == "__main__":
-    app.config["SQLALCHEMY_DATABASE_URI"] = config.getAppConfig("SQLALCHEMY_DATABASE_URI").format(environ.get("MASK_PASSWORD", "root:root"))
+    app.config["SQLALCHEMY_DATABASE_URI"] = \
+        config.getAppConfig("SQLALCHEMY_DATABASE_URI")\
+        .format(environ.get("MASK_PASSWORD", "root:root"))
     useDebug = config.getAppConfig("3DMASK_LANDSCAPE") == "sandbox"
     usePort = 5000 if useDebug else None
     config.setupLogger()
     print("\n" * 1)
-    logging.warning('=== Launching 3DMask Web API ({}) ==='.format(config.getLandscape()))
-    app.run(debug=useDebug, use_reloader=True, port=usePort)
+    logging.warning('=== Launching 3DMask Web API ({}) ==='
+                    .format(config.getLandscape()))
+    app.run(debug=useDebug, use_reloader=True, port=usePort, host="0.0.0.0")
