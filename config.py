44ecd562151abd9539756c9ce3cd16ffec7bd096
Pedro <pfonseca@synopsys.com>
2020-03-29 03:31:36 +0100

    Now linted with flake8

--- config.py
+++ config.py
@@ -2,20 +2,12 @@ from sys import platform
 from os import environ
 import pathlib
 import sys
-from flask import (
-    Flask
-    ,make_response
-    ,jsonify
-    )
-import jinja2
 import logging
-from flask_jwt import JWT
-from datetime import timedelta
-import logging
-from logging.handlers import TimedRotatingFileHandler
-import json
 from flask_sqlalchemy import SQLAlchemy
-
+from flask import (
+    Flask,
+    make_response
+)
 print("= config =" * 3)
 
 logger = logging.getLogger(__name__)
@@ -35,6 +27,7 @@ app.config["STR_SUCCESS"] = "success"
 app.config["STR_FAILURE"] = "failure"
 
 db = SQLAlchemy(app)
+
 
 def setupLogger():
 
@@ -56,95 +49,96 @@ def setupLogger():
     handlers = [fileHandler, stdoutHandler]
 
     logging.basicConfig(
-        level=logging.INFO
-        ,format = "%(asctime)s||%(levelname)s||%(filename)s||%(lineno)s||%(funcName)s()||%(message)s"
-        ,datefmt = '%m/%d/%Y %I:%M:%S %p'
-        ,handlers=handlers        
+        level=logging.INFO,
+        format="%(asctime)s||%(levelname)s||%(filename)s||%(lineno)s||%(funcName)s()||%(message)s",
+        datefmt='%m/%d/%Y %I:%M:%S %p',
+        handlers=handlers
     )
 
     logger = logging.getLogger(__name__)
     logger.warn("Logging under {}".format(platform))
     logger.warn("Log file is [{}]".format(_logFileFQN))
 
+
 def getAppConfig(pName):
 
     ret = None
-    
+
     try:
 
-        ret = app.config[("{}_{}".format(pName,getLandscape())).upper()]
+        ret = app.config[("{}_{}".format(pName, getLandscape())).upper()]
 
     except Exception:
-        
         ret = app.config[pName]
-            
+
     finally:
         return ret
+
 
 def getLandscape():
 
     _env = environ.get("MASK_LANDSCAPE", "sandbox").lower()
-    if _env in ("dev","qa","prd","sandbox"):
+    if _env in ("dev", "qa", "prd", "sandbox"):
         return _env
     else:
-        return "sandbox" 
+        return "sandbox"
+
 
 class Tester(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    msg = db.Column(db.String(80), unique = False)
+    id = db.Column(db.Integer, primary_key=True)
+    msg = db.Column(db.String(80), unique=False)
 
-    def __init__ (self,msg):
+    def __init__(self, msg):
         self.msg = msg
-    
+
     def toJSON(self):
-           
+
         json = {
-            "id"           : str(self.id),
-            "msg"          : self.msg
+            "id": str(self.id),
+            "msg": self.msg
         }
 
         return json
+
 
 class Result():
 
     SUCCESS = "success"
     FAILURE = "failure"
     status = ""
-    #message = ""
+    # message = ""
     data = []
 
-    def __init__(self,**kwargs):
+    def __init__(self, **kwargs):
 
         data = kwargs.get("data", [])
 
         if type(data) != list:
-            data = [ data ]
+            data = [data]
 
         self.status = kwargs.get("status", "failure")
         self.data = data
-        #self.message = kwargs.get("message", "{} rows".format(self.dataCount()))
+        # self.message = kwargs.get("message", "{} rows".format(self.dataCount()))
 
     def noDataFound(self):
         return len(self.data) == 0
 
     def dataCount(self):
-        #print(self.data)
+        # print(self.data)
         return len(self.data)
 
     def toJSON(self):
 
         _data = self.data
-        
+
         if not self.data:
             _data = None
-        
+
         return {
             "status": self.status,
-            #"message": self.message,
+            # "message": self.message,
             "data": _data
         }
 
     def response(self):
-        return make_response(self.toJSON(),200)
-
-
+        return make_response(self.toJSON(), 200)
