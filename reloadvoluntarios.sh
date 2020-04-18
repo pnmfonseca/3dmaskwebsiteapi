90f258397bda41279128d0d137f1fee6eed46a4b
Pedro <pfonseca@synopsys.com>
2020-04-18 01:15:52 +0100

    Small adjustment on reloadvoluntarios.sh

--- reloadvoluntarios.sh
+++ reloadvoluntarios.sh
@@ -2,5 +2,7 @@
 
 logfile=reload.log
 now=$(date +%Y%m%d\ %H:%M:%S)
+maskToken="TYPE-TOKEN-HERE"
 echo "Reloading at $now" >> $logfile
-curl -s -H "Authorization: $MASK_TOKEN" -X POST $URL/voluntario/reload >> $logfile #2>&1
+
+curl -H "Authorization: $maskToken" -X POST $URL/voluntario/reload >> $logfile 2>&1
